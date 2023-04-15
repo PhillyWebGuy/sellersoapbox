aa69cc0c1e1067bfd57519f1a7f57988ea4b6813
Ed Hertzog <edhertzog@gmail.com>
2023-04-15 08:46:57 -0400

    Initial commit

---
+++ Themes/default/languages/Modifications.english-utf8.php
@@ -0,0 +1,11 @@
+<?php
+// Version: 2.0; Modifications
+
+
+$txt['youtube'] = 'YouTube';
+$txt['youtube_invalid'] = '#Invalid YouTube Link#';
+
+$txt['youtube'] = 'YouTube';
+$txt['youtube_invalid'] = '#Invalid YouTube Link#';
+
+?>
---
+++ Themes/default/languages/Modifications.english.php
@@ -0,0 +1,10 @@
+<?php
+// Version: 2.0; Modifications
+
+$txt['youtube'] = 'YouTube';
+$txt['youtube_invalid'] = '#Invalid YouTube Link#';
+
+$txt['youtube'] = 'YouTube';
+$txt['youtube_invalid'] = '#Invalid YouTube Link#';
+
+?>
